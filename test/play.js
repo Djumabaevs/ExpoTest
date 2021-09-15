ba65c09e8e391d92ccfde2c54d3c376a1eeefa52
Djumabaevs <djumabaevb@gmail.com>
2021-09-15 15:31:14 +0600

    starting add features to app

--- test/play.js
+++ test/play.js
@@ -42,12 +42,15 @@ const fetchData = () => {
 
 setTimeout(() => {
     console.log('Timer is done!')
-    fetchData().then(text => {
+    fetchData()
+    .then(text => {
         console.log(text)
-        return fetchData
+        return fetchData()
     })
     .then(text2 => {
         console.log(text2)
     })
 }, 2000)
+
+
 
